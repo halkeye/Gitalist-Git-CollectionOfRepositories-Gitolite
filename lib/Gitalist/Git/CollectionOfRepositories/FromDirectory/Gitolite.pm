ea6c35ac317ad0af3bf67a4726c325554b5a56cd
Gavin <gavin@kodekoan.com>
2012-04-06 10:12:51 -0700

    new version using Vhost as an example

--- lib/Gitalist/Git/CollectionOfRepositories/FromDirectory/Gitolite.pm
+++ lib/Gitalist/Git/CollectionOfRepositories/FromDirectory/Gitolite.pm
@@ -1,7 +1,96 @@
-package Gitalist::Git::CollectionOfRepositories::FromDirectory::Gitolite;
-
-use strict;
+use MooseX::Declare;
+use IPC::System::Simple ();
 use warnings;
+use strict;
+use Data::Dumper;
+use File::Basename;
 
+# GL_RC=/home/git/.gitolite.rc GL_BINDIR=/home/git/bin HOME=/home/git perl -I /home/git/bin -Mgitolite_env -Mgitolite -e 'print gitolite::report_basic("^","halkeye");'
+$ENV{GL_RC}||="/home/git/.gitolite.rc";
+$ENV{GL_BINDIR}||="/home/git/bin";
 
-1;
+class Gitalist::Git::CollectionOfRepositories::FromDirectory::Gitolite
+    with Gitalist::Git::CollectionOfRepositoriesWithRequestState {
+
+    method extract_request_state ($ctx) {
+        return (remote_user => $ctx->request->remote_user);
+    }
+}
+class Gitalist::Git::CollectionOfRepositories::FromDirectory::GitoliteImpl
+    extends Gitalist::Git::CollectionOfRepositories {
+    use MooseX::Types::Moose qw/ HashRef Str /;
+    use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
+    use MooseX::Types::Path::Class qw/Dir/;
+    use Moose::Util::TypeConstraints;
+
+    has remote_user_dispatch => (
+        isa => HashRef,
+        traits => ['Hash'],
+        required => 1,
+        handles => {
+            _get_collection_name_for_vhost => 'get',
+        },
+    );
+
+    has collections => (
+        isa => HashRef,
+        traits => ['Hash'],
+        required => 1,
+        handles => {
+            _get_collection => 'get',
+        }
+    );
+
+    has remote_user => (
+        is => 'ro',
+        isa => Str,
+        required => 1,
+    );
+
+    method debug_string { 'chosen collection ' . ref($self->chosen_collection) . " " . $self->chosen_collection->debug_string }
+
+    role_type 'Gitalist::Git::CollectionOfRepositories';
+    has chosen_collection => (
+        is => 'ro',
+        does => 'Gitalist::Git::CollectionOfRepositories',
+        handles => [qw/
+            _get_repo_from_name
+            _build_repositories
+            /],
+        default => sub {
+            my $self = shift;
+            my $ret = [];
+            my $user = ($self->remote_user || 'guest');
+            $user =~ s/\@.*$//; # trim off exchange domain
+            $user = lc $user;
+
+            local $ENV{HOME} = File::Basename::dirname($ENV{GL_RC});
+
+            # Gitolite does a lot of messing with envs so only load at runtime once everything is setup right (better for local)
+            unless ($INC{'gitolite'})
+            {
+                no warnings;
+                require "$ENV{GL_BINDIR}/gitolite_env.pm";
+            }
+
+            my @repos;
+            eval {
+                {
+                    no warnings;
+                    @repos = gitolite::list_phy_repos();
+                }
+
+                foreach my $repo ( sort { lc $a cmp lc $b } @repos )
+                {
+                    my $dir = $self->repo_dir->subdir($repo . '.git');
+                    next unless -d $dir; 
+                    next unless gitolite::can_read( $repo, $user);
+                    push @$ret, Gitalist::Git::Repository->new($dir),
+                }
+            };
+            warn $@ if $@;
+            return $ret;
+        },
+        lazy => 1,
+    );
+}
